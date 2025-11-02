5051c921f04da1dcbe75ea21109939a9e9df3ab3
nandothor19 <nandothor19@gmail.com>
2025-11-02 00:59:53 -0300

    Mudanças da atividade da agenda 13

---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		App.Current.MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+    }
+}
